f263d4edb18b1811d2a089258b4130e2fdb4b53f
HiepNguyenn2002 <nguyenhiepnguyen2002@gmail.com>
2024-07-10 15:22:14 +0700

    api

---
+++ PetCareSystem/PetCareSystem/Models/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace PetCareSystem.Models;
+
+public class Room : BaseEntity
+{
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public decimal Price { get; set; }
+
+	public IEnumerable<PetRoom> PetRooms { get; set; }
+}
